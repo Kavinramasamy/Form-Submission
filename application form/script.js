7984dc78b7ab1879a8bb39f8440fbbd3b03a08d8
Kavinramasamy <124972388+Kavinramasamy@users.noreply.github.com>
2023-10-28 12:39:44 +0530

    Update script.js

--- application form/script.js
+++ application form/script.js
@@ -1,5 +1,4 @@
-var selectTag=document.getElementById("country-state");
-var fname = document.getElementById("fname");
+ar fname = document.getElementById("fname");
 var lname = document.getElementById("lname");
 var address1 = document.getElementById("address1");
 var address2 = document.getElementById("address2");
@@ -10,7 +9,7 @@ var state = document.getElementById("state");
 var country = document.getElementById("country");
 
 
-function tablecontent(fname,lname,address,pincode,gender,food,state,country){
+function tablecontent(fname, lname, address, pincode, gender, food, state, country) {
     var tbody = document.getElementById("tbody");
     var tr = document.createElement("tr");
     var td1 = document.createElement("td");
@@ -29,49 +28,49 @@ function tablecontent(fname,lname,address,pincode,gender,food,state,country){
     td6.innerHTML = food;
     td7.innerHTML = state;
     td8.innerHTML = country;
-    tr.append(td1,td2,td3,td4,td5,td6,td7,td8);
+    tr.append(td1, td2, td3, td4, td5, td6, td7, td8);
     tbody.append(tr);
 }
 
 
-var submit=document.getElementById("submit");
-submit.addEventListener("click",function(event){
+var submit = document.getElementById("submit");
+submit.addEventListener("click", function (event) {
     event.preventDefault();
-    var address=[];
+    var address = [];
     address.push(address1.value);
     address.push(address2.value);
 
-    var gender=document.getElementsByName("gender");
+    var gender = document.getElementsByName("gender");
     var gender1;
-    for(let i=0;i<gender.length;i++){
-        if(gender[i].checked){
-            gender1 =gender[i].value;
+    for (let i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            gender1 = gender[i].value;
         }
     }
-    
-    var food=document.getElementsByName("food");
-    var foodList=[];
-    var Count=0;
-    for(let i=0;i<food.length;i++){
-        if(food[i].checked){
+
+    var food = document.getElementsByName("food");
+    var foodList = [];
+    var Count = 0;
+    for (let i = 0; i < food.length; i++) {
+        if (food[i].checked) {
             foodList.push(food[i].value);
             Count++;
         }
     }
-    if(foodList.length<=2){
-        value=foodList.join(", ");
-    }else{
-        value=alert("Choose atleast 2 options out of 5 in food");
+    if (foodList.length >= 2) {
+        value = foodList.join(", ");
+    } else {
+        value = alert("Choose atleast 2 options out of 5 in food");
     }
 
-    tablecontent(fname.value,lname.value,address.join(", "),pin.value,gender1,value,state.value,country.value)
-    fname.value="";
-    lname.value="";
-    address1.value="";
-    address2.value="";
-    pin.value="";
-    gender.value="";
-    foodList=[];
-    state.value="";
-    country.value="";
+    tablecontent(fname.value, lname.value, address.join(", "), pin.value, gender1, value, state.value, country.value)
+    fname.value = "";
+    lname.value = "";
+    address1.value = "";
+    address2.value = "";
+    pin.value = "";
+    gender.value = "";
+    foodList = [];
+    state.value = "";
+    country.value = "";
 })
